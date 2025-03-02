5a80090c76834e315cd32de05d01071d96d5f458
jakub-ozog <jakubjanozog@gmail.com>
2025-03-02 18:49:13 +0100

    feat(deploy GH scripts): adding required scripts for github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -12,7 +17,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
     },
   },
+  base: import.meta.env.MODE === 'production' ?
+    '/bizony-rzeszow/' // Dodaj nazwę swojego repozytorium
+    :
+    '/',
 })
