882841cc2bfc8edd8b29b8912d28b5588cb371e4
jakub-ozog <jakubjanozog@gmail.com>
2025-03-26 21:59:42 +0100

    feat: add HomeTable component with homeTable.js data object to the HomeView as a table

---
+++ src/components/HomeTable.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { homeTable } from '@/data/homeTable.js';
+</script>
+
+<template>
+  <div class="my-16">
+    <DataTable :value="homeTable" stripedRows showGridlines tableStyle="min-width: 50rem" class="my-12">
+      <div class="text-left w-full bg-blue-800 text-white p-6 rounded-t-lg ">
+        <h3 class="text-white text-2xl font-semibold">Grupa C</h3>
+      </div>
+      <PrimeColumn field="poz" header="poz"></PrimeColumn>
+      <PrimeColumn field="druzyna" header="druzyna">
+        <template #body="slotProps">
+          <div class="flex items-center gap-3 flex-wrap">
+            <img :src="slotProps.data.druzyna.logo" alt="" class="w-16 h-auto">
+            <p class="text-xl">
+              {{ slotProps.data.druzyna.nazwa }}
+            </p>
+          </div>
+        </template>
+      </PrimeColumn>
+      <PrimeColumn field="mecze" header="mecze"></PrimeColumn>
+      <PrimeColumn field="runyPlus" header="runyPlus"></PrimeColumn>
+      <PrimeColumn field="runyMinus" header="runyMinus"></PrimeColumn>
+      <PrimeColumn field="zwyciestwa" header="zwyciestwa"></PrimeColumn>
+      <PrimeColumn field="remisy" header="remisy"></PrimeColumn>
+      <PrimeColumn field="przegrane" header="przegrane"></PrimeColumn>
+      <PrimeColumn field="punkty" header="punkty"></PrimeColumn>
+    </DataTable>
+  </div>
+</template>
---
+++ src/data/homeTable.js
@@ -0,0 +1,80 @@
+import BizonyLogo from '@/assets/logo/bizony.webp'
+import KingsLogo from '@/assets/logo/kings.webp'
+import RawaLogo from '@/assets/logo/rawa.webp'
+import SilesiaLogo from '@/assets/logo/rybnik.webp'
+import WizardsLogo from '@/assets/logo/wizards.webp'
+
+export const homeTable = [
+  {
+    poz: 1,
+    druzyna: {
+      nazwa: 'Bizony Rzeszów',
+      logo: BizonyLogo, // Załóżmy, że masz logo o takiej nazwie
+    },
+    mecze: 0,
+    runyPlus: 0,
+    runyMinus: 0,
+    zwyciestwa: 0,
+    remisy: 0,
+    przegrane: 0,
+    punkty: 0,
+  },
+  {
+    poz: 2,
+    druzyna: {
+      nazwa: 'Kraków Kings',
+      logo: KingsLogo, // Załóżmy, że masz logo o takiej nazwie
+    },
+    mecze: 0,
+    runyPlus: 0,
+    runyMinus: 0,
+    zwyciestwa: 0,
+    remisy: 0,
+    przegrane: 0,
+    punkty: 0,
+  },
+  {
+    poz: 3,
+    druzyna: {
+      nazwa: 'Rawa Katowice',
+      logo: RawaLogo, // Załóżmy, że masz logo o takiej nazwie
+    },
+    mecze: 0,
+    runyPlus: 0,
+    runyMinus: 0,
+    zwyciestwa: 0,
+    remisy: 0,
+    przegrane: 0,
+    punkty: 0,
+  },
+  {
+    poz: 4,
+    druzyna: {
+      nazwa: 'Silesia Rybnik',
+      logo: SilesiaLogo, // Załóżmy, że masz logo o takiej nazwie
+    },
+    mecze: 0,
+    runyPlus: 0,
+    runyMinus: 0,
+    zwyciestwa: 0,
+    remisy: 0,
+    przegrane: 0,
+    punkty: 0,
+  },
+  {
+    poz: 5,
+    druzyna: {
+      nazwa: 'Wizards Opole',
+      logo: WizardsLogo, // Załóżmy, że masz logo o takiej nazwie
+    },
+    mecze: 0,
+    runyPlus: 0,
+    runyMinus: 0,
+    zwyciestwa: 0,
+    remisy: 0,
+    przegrane: 0,
+    punkty: 0,
+  },
+]
+
+export default homeTable
