de881d34508076f74ccfac4716dcca9fd1e03d6d
jakub-ozog <jakubjanozog@gmail.com>
2025-04-08 19:11:18 +0200

    chore: countdown timer update

---
+++ src/components/CountdownWatch.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+
+
+const countdown = ref({
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+})
+
+const matchDates = [
+  { date: new Date('2025-04-13T11:00:00') },
+  { date: new Date('2025-06-08T11:00:00') },
+  { date: new Date('2025-06-15T11:00:00') },
+]
+
+
+const currentIndex = ref(0)
+const targetDate = ref(matchDates[currentIndex.value].date)
+
+let interval = null
+
+const updateCountdown = () => {
+  const now = new Date()
+  const diff = targetDate.value - now
+
+  if (diff <= 0) {
+    currentIndex.value++
+    if (currentIndex.value < matchDates.length) {
+      targetDate.value = matchDates[currentIndex.value].date
+    } else {
+      clearInterval(interval)
+      countdown.value = { days: 0, hours: 0, minutes: 0, seconds: 0 }
+    }
+    return
+  }
+
+  const seconds = Math.floor((diff / 1000) % 60)
+  const minutes = Math.floor((diff / 1000 / 60) % 60)
+  const hours = Math.floor((diff / 1000 / 60 / 60) % 24)
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24)
+
+  countdown.value = { days, hours, minutes, seconds }
+}
+
+onMounted(() => {
+  updateCountdown()
+  interval = setInterval(updateCountdown, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(interval)
+})
+
+
+</script>
+
+<template>
+
+
+
+
+
+  <div class=" mx-auto flex flex-col justify-center items-center">
+    <p class="text-xl font-bold text-white">Do najbliższego meczu pozostało</p>
+    <div class="flex items-center justify-center gap-8 mt-8  w-[300px]">
+      <div class="flex flex-col items-center text-white">
+        <p class="text-5xl font-bold text-white  w-[38px]">{{ countdown.days }}</p>
+        <p class="text-sm font-light text-white">dni</p>
+      </div>
+      <p class="text-white text-2xl font-bold">:</p>
+      <div class="flex flex-col items-center">
+        <p class="text-5xl font-bold text-white  w-[38px]"> {{ countdown.hours }}</p>
+        <p class="text-sm font-light text-white">godzin</p>
+      </div>
+      <p class="text-white text-2xl font-bold">:</p>
+      <div class="flex flex-col items-center">
+        <p class="text-5xl font-bold text-white  w-[38px]">{{ countdown.minutes }}</p>
+        <p class="text-sm font-light text-white">minut</p>
+      </div>
+      <p class="text-white text-2xl font-bold">:</p>
+      <div class="flex flex-col items-center">
+        <p class="text-5xl font-bold text-white  w-[38px]">{{ countdown.seconds }}</p>
+        <p class="text-sm font-light text-white">sekund</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/UpcomingMatchCounter.vue
+++ src/components/UpcomingMatchCounter.vue
@@ -1,33 +1,11 @@
 <script setup>
 import MatchCounterBg from '@/assets/images/match-counter-bg.webp'
+import CountdownWatch from './CountdownWatch.vue';
 </script>
 
 <template>
   <section class="w-full text-white min-h-[20rem] flex bg-cover bg-center relative font-urbanist"
     :style="{ backgroundImage: `url(${MatchCounterBg})` }">
-    <div class="container mx-auto flex flex-col justify-center items-center">
-      <p class="text-xl font-bold text-white">Do najbliższego meczu pozostało</p>
-      <div class="flex items-center justify-center gap-4 mt-4 ">
-        <div class="flex flex-col items-center text-white">
-          <p class="text-4xl font-bold text-white">75</p>
-          <p class="text-sm font-light text-white">dni</p>
-        </div>
-        <p>:</p>
-        <div class="flex flex-col items-center">
-          <p class="text-4xl font-bold text-white">12</p>
-          <p class="text-sm font-light text-white">godzin</p>
-        </div>
-        <p>:</p>
-        <div class="flex flex-col items-center">
-          <p class="text-4xl font-bold text-white">45</p>
-          <p class="text-sm font-light text-white">minut</p>
-        </div>
-        <p>:</p>
-        <div class="flex flex-col items-center">
-          <p class="text-4xl font-bold text-white">25</p>
-          <p class="text-sm font-light text-white">sekund</p>
-        </div>
-      </div>
-    </div>
+    <CountdownWatch />
   </section>
 </template>
