36268150bfb95d4bbc30e741748b6f1477e6fb9f
jakub-ozog <jakubjanozog@gmail.com>
2025-05-20 19:16:38 +0200

    chore: gallery slider height update

--- src/components/galleries/GalleryEl7.vue
+++ src/components/galleries/GalleryEl7.vue
@@ -38,17 +38,18 @@ const photos = Object.values(images).map((img) => img.default);
 
     <swiper :spaceBetween="10" :navigation="true" :thumbs="{ swiper: thumbsSwiper }" :modules="modules" :pagination="{
       type: 'fraction',
-    }" class="">
+    }" class="max-w-[1000px]">
       <swiper-slide class="h-unset" v-for="(photo, index) in photos" :key="index">
         <img :src="photo" class='cursor-grab select-none h-full' />
       </swiper-slide>
     </swiper>
     <swiper @swiper="setThumbsSwiper" :loop="true" :spaceBetween="5" :slidesPerView="4" :watchSlidesProgress="true"
-      :modules="modules" class="mySwiper">
-      <swiper-slide class="opacity-75 mt-8 h-unset" v-for="(photo, index) in photos" :key="index">
-        <img :src="photo" class='cursor-grab select-none h-full' />
+      :modules="modules" class="mySwiper max-w-[1000px]">
+      <swiper-slide class="opacity-75 mt-8 " v-for="(photo, index) in photos" :key="index">
+        <img :src="photo" class='cursor-grab select-none ' />
       </swiper-slide>
     </swiper>
+
 
 
   </section>
